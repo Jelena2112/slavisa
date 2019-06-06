0394697974e93eef87ef4ffe789e4d5145951a09
jelena2112 <dijanic@gmail.com>
2019-06-06 21:34:37 +0200

    *

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.min.css" />
     </head>
 
     <body>
