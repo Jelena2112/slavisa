9e8ee05c3e2d3259e7046b097fdc704e0a9d1f4f
jelena2112 <dijanic@gmail.com>
2019-06-07 15:06:20 +0200

    home responsive

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,18 +3,25 @@
 @section("content")
    <section class="home flex flexCenter">
        <form class="homeForm flex flexCenter">
-           <p class="formHome">Check-in:</p>
-           <input  class="formHome" type="date" name="start" placeholder="check-in date">
 
-           <p class="formHome">Check-out:</p>
-           <input  class="formHome" type="date" name="end">
+           <span class="wrapForm flex flexCenter">
+               <p class="formHome text">Check-in:</p>
+               <input  class="formHome" type="date" name="start" placeholder="check-in date">
+           </span>
 
-           <p class="formHome">For:</p>
-           <select class="formHome select">
-               <option value="2">2 persons</option>
-               <option value="3">3 persons</option>
-               <option value="4">4 persons</option>
-           </select>
+           <span class="wrapForm flex flexCenter">
+               <p class="formHome text">Check-out:</p>
+               <input  class="formHome" type="date" name="end">
+           </span>
+
+           <span class="wrapForm flex flexCenter">
+               <p class="formHome text">For:</p>
+               <select class="formHome select">
+                   <option value="2">2 persons</option>
+                   <option value="3">3 persons</option>
+                   <option value="4">4 persons</option>
+               </select>
+           </span>
 
            <input class="formHome submit" type="submit">
 
