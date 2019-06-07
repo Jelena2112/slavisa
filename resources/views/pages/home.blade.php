f9b6fb7f690b7464c9c0f890865b00aa31189d99
jelena2112 <dijanic@gmail.com>
2019-06-07 14:47:22 +0200

    home form details added

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,24 +3,23 @@
 @section("content")
    <section class="home flex flexCenter">
        <form class="homeForm flex flexCenter">
-           <p class="formHome">From:</p>
-           <input  class="formHome" type="date" name="start">
+           <p class="formHome">Check-in:</p>
+           <input  class="formHome" type="date" name="start" placeholder="check-in date">
 
-           <p class="formHome">To:</p>
+           <p class="formHome">Check-out:</p>
            <input  class="formHome" type="date" name="end">
 
            <p class="formHome">For:</p>
-           <select class="formHome">
+           <select class="formHome select">
                <option value="2">2 persons</option>
                <option value="3">3 persons</option>
                <option value="4">4 persons</option>
            </select>
 
+           <input class="formHome submit" type="submit">
+
        </form>
 
    </section>
-
-
-
 
 @endsection("content")
