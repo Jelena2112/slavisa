afbd1127821f2b69e99fe8228f6c3c72f00ce8dd
jelena2112 <dijanic@gmail.com>
2019-06-07 13:58:41 +0200

    responsive for hom and nav added

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -2,12 +2,15 @@
     <div class="navDiv flex">
         <a class="navIcon link" href="#">
         </a>
-        <div class="navMain flex">
+        <div class="navMain flex fontColorWhite">
             <a class="navbarItem link">Home</a>
             <a class="navbarItem link">home</a>
             <a class="navbarItem link">home</a>
             <a class="navbarItem link">home</a>
             <a class="navbarItem link">contac</a>
         </div>
+        <a class="mobNav">
+            <i class="fas fa-bars fontColorWhite"></i>
+        </a>
     </div>
 </nav>
