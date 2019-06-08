67cf549cd85d8d0fd70dbf69cfc652bb19d75c0d
jelena2112 <dijanic@gmail.com>
2019-06-08 21:41:48 +0200

    js  checking if nav is already clicked

--- public/js/style.js
+++ public/js/style.js
@@ -1,32 +1,41 @@
 $(document).ready(function (){
 
     var navDown = false;
+    var windowWidth = $(window).width();
+    var navClick = false;
 
     $(".mobNav").click(function (){
-        if(!navDown){
-            $(".navMain").css({"display":"flex", "height":"0px"}).animate({"height":"250px"});
-            $(".fa-bars").hide();
-            $(".mobNav2").show();
-            navDown = true;
+        if(!navClick){
+            navClick = true;
+            if(!navDown){
+                $(".navMain").css({"display":"flex", "height":"0px"}).animate({"height":"250px"}, function() {
+                    navClick = false;
+                });
+                $(".fa-bars").hide();
+                $(".mobNav2").show();
+                navDown = true;
+            }
+            else if(navDown){
+                $(".navMain").slideUp();
+                $(".mobNav2").hide();
+                $(".fa-bars").show();
+                navDown = false;
+                navClick = false;
+            }
         }
-        else if(navDown){
-            $(".navMain").slideUp();
-            $(".mobNav2").hide();
-            $(".fa-bars").show();
-            navDown = false;
-        }
-
 
     });
 
     $(window).scroll(function (){
-        if($(window).scrollTop() > 100 ){
-            $(".navbar").css({"background-color":"white"});
-            $(".navbarItem").css({"color":"black"});
-        }
-        else{
-            $(".navbar").css({"background-color":"transparent"});
-            $(".navbarItem").css({"color":"white"});
+        if(windowWidth > 1024){
+            if($(window).scrollTop() > 100 ){
+                $(".navbar").css({"background-color":"white"});
+                $(".navbarItem").css({"color":"black"});
+            }
+            else{
+                $(".navbar").css({"background-color":"transparent"});
+                $(".navbarItem").css({"color":"white"});
+            }
         }
 
     });
