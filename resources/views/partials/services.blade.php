f5575302eb377ecdfcc48fa52901c256e31eea49
jelena2112 <dijanic@gmail.com>
2019-06-10 15:38:11 +0200

    services finished all

---
+++ resources/views/partials/services.blade.php
@@ -0,0 +1,76 @@
+<div class="services flex flexCenter">
+
+    <section class="servicesSection servicesMain">
+
+        <h1 class="servicesHeader">OUR SERVICES</h1>
+
+        <p class="servicesText">CASTLE “KASTIL JELA” in Milna provides adults-only accommodation with a garden, a bar and a shared lounge. 2.9 km from Lucice Bay and 29 km from Blaca Desert, the hotel offers a private beach area. The air-conditioned rooms provide a garden view and come with a desk and free WiFi.</p><br>
+
+        <p class="servicesText">All guest rooms at the hotel come with a seating area, a flat-screen TV with satellite channels and a private bathroom with a hair dryer and a bath. All units will provide guests with a fridge.</p><br>
+
+        <p class="servicesText">CASTLE “KASTIL JELA” offers a terrace. The area is popular for cycling, and bike hire is available at the accommodation.</p><br>
+
+        <p class="servicesText">Staff speak Italian, English, German and Croatian at the 24-hour front desk.</p>
+
+    </section>
+
+    <section class="servicesSection flex flexCenter">
+
+        <article class="itemArticle">
+
+            <h2 class="servicesItems"><i class="fas fa-concierge-bell servicesIcon"></i>24-hour front desk</h2>
+
+            <p class="servicesText textItem">Staff speak Italian, English, German and Croatian at the 24-hour front desk.</p>
+
+        </article>
+
+        <article class="itemArticle">
+
+            <h2 class="servicesItems"><i class="fas fa-parking servicesIcon"></i>Free parking</h2>
+
+            <p class="servicesText textItem">Free parking for our guests.</p>
+
+        </article>
+
+        <article class="itemArticle">
+
+            <h2 class="servicesItems"><i class="fas fa-umbrella-beach servicesIcon"></i>Private beach area</h2>
+
+            <p class="servicesText textItem">CASTLE “KASTIL JELA” in Milna provides adults-only accommodation with a garden, a bar and a shared lounge. 2.9 km from Lucice Bay and 29 km from Blaca Desert, the hotel offers a private beach area.</p>
+
+        </article>
+
+    </section>
+
+
+    <section class="servicesSection flex flexCenter">
+
+        <article class="itemArticle">
+
+            <h2 class="servicesItems"><i class="fas fa-wifi servicesIcon"></i>Free WiFi</h2>
+
+            <p class="servicesText textItem">The air-conditioned rooms provide a garden view and come with a desk and free WiFi.</p>
+
+        </article>
+
+        <article class="itemArticle">
+
+            <h2 class="servicesItems"><i class="fas fa-glass-martini-alt servicesIcon"></i>Bar</h2>
+
+            <p class="servicesText textItem">Free parking for our guests.</p>
+
+        </article>
+
+        <article class="itemArticle">
+
+            <h2 class="servicesItems"><i class="fas fa-tv servicesIcon"></i>Flat-screen TV</h2>
+
+            <p class="servicesText textItem">All guest rooms at the hotel come with a seating area, a flat-screen TV with satellite channels.</p>
+
+        </article>
+
+    </section>
+
+
+
+</div>
