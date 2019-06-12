0824bdc29fd855215a065216d123f202fbd1a20c
jelena2112 <dijanic@gmail.com>
2019-06-12 12:21:44 +0200

    apartments done

--- resources/views/partials/apartments.blade.php
+++ resources/views/partials/apartments.blade.php
@@ -6,11 +6,12 @@
         <article class="apartmentsArticle flex">
             <img class="image" src="images/milna.jpg" alt="image">
             <div class="apartmentDiv">
-                <h2 class="apartmentsRoom">APARTMENT</h2>
+                <h2 class="apartmentsRoom">DELUXE KING STUDIO</h2>
                 <ul class="apartmentUl">
-                    <li class="apartmentList">Room</li>
-                    <li class="apartmentList">Room</li>
-                    <li class="apartmentList">Room</li>
+                    <li class="apartmentList">Room size 33 m²</li>
+                    <li class="apartmentList">1 large double bed</li>
+                    <li class="apartmentList">Patio</li>
+                    <li class="apartmentList">Private bathroom</li>
                 </ul>
             </div>
         </article>
@@ -18,11 +19,12 @@
         <article class="apartmentsArticle flex">
             <img class="image" src="images/milna.jpg" alt="image">
             <div class="apartmentDiv">
-                <h2 class="apartmentsRoom">APARTMENT</h2>
+                <h2 class="apartmentsRoom">DELUXE SUITE WITH SPA BATH</h2>
                 <ul class="apartmentUl">
-                    <li class="apartmentList">Room</li>
-                    <li class="apartmentList">Room</li>
-                    <li class="apartmentList">Room</li>
+                    <li class="apartmentList">Room size 33 m²</li>
+                    <li class="apartmentList">1 extra-large double bed</li>
+                    <li class="apartmentList">Terrace</li>
+                    <li class="apartmentList">Private bathroom</li>
                 </ul>
             </div>
         </article>
@@ -30,11 +32,13 @@
         <article class="apartmentsArticle flex">
             <img class="image" src="images/milna.jpg" alt="image">
             <div class="apartmentDiv">
-                <h2 class="apartmentsRoom">APARTMENT</h2>
+                <h2 class="apartmentsRoom">SUPERIOR TRIPLE ROOM WITH SEA VIEW</h2>
                 <ul class="apartmentUl">
-                    <li class="apartmentList">Room</li>
-                    <li class="apartmentList">Room</li>
-                    <li class="apartmentList">Room</li>
+                    <li class="apartmentList">Room size 50 m²</li>
+                    <li class="apartmentList">1 single bed</li>
+                    <li class="apartmentList">1 double extra large bed</li>
+                    <li class="apartmentList">Terrace</li>
+                    <li class="apartmentList">Private bathroom</li>
                 </ul>
             </div>
         </article>
