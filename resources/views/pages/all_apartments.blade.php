4c65ce46cf2242b2870ceff79021ec7f6da1fd74
jelena2112 <dijanic@gmail.com>
2019-06-17 17:30:50 +0200

    all apartments corrected

--- resources/views/pages/all_apartments.blade.php
+++ resources/views/pages/all_apartments.blade.php
@@ -4,45 +4,85 @@
 
     <section class="all_ap_section flex flexCenter">
 
-        <article class="all_ap flex flexCenter">
-
             <a class="allDiv link" href="#">
                 <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
             </a>
 
             <a class="allDiv link" href="#">
                 <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
             </a>
 
             <a class="allDiv link" href="#">
                 <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
             </a>
 
             <a class="allDiv link" href="#">
                 <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
-            </a>
-
-        </article>
-
-        <article class="all_ap flex flexCenter">
-
-            <a class="allDiv link" href="#">
-                <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
-            </a>
-
-            <a class="allDiv link" href="#">
-                <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
             </a>
 
             <a class="allDiv link" href="#">
                 <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
             </a>
 
             <a class="allDiv link" href="#">
                 <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
             </a>
 
-        </article>
+            <a class="allDiv link" href="#">
+                <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
+            </a>
+
+            <a class="allDiv link" href="#">
+                <h3 class="allName">Delux Studio sa King-Sajz Krevetom</h3>
+                <img class="" src="../images/milna.jpg">
+                <p class="allText">Room size 33 m²</p>
+                <p class="allText"><i class="fas fa-user-alt allAp"></i><i class="fas fa-user-alt"></i>For:2 adults</p>
+                <p class="allText"><i class="fas fa-shower allAp"></i>Private bathroom</p>
+                <p class="allText"><i class="fas fa-tv allAp"></i>Flatt screen TV</p>
+                <p class="allText"><i class="fas fa-wind allAp"></i>Air conditioning</p>
+            </a>
 
     </section>
 
